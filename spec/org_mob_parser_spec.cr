eaff5f36037ab7f76f15e5ec70ffddedf4672763
roy-corentin <corentin.roy@epitech.eu>
2023-03-14 10:10:38 +0100

    [ADD] specs to check type return by the parser

--- spec/org_mob_parser_spec.cr
+++ spec/org_mob_parser_spec.cr
@@ -1,9 +1,37 @@
 require "./spec_helper"
 
-describe OrgMobParser do
-  # TODO: Write tests
+describe OrgMob::Parser do
+  describe "#self.parse" do
+    context "parse title" do
+      it "should have title as type" do
+        OrgMob::Parser.parse("* Title").first[:type].should eq("title")
+      end
+    end
 
-  it "works" do
-    false.should eq(true)
+    context "parse list" do
+      context "dash list" do
+        it "should have list as type" do
+          OrgMob::Parser.parse("- List").first[:type].should eq("list")
+        end
+      end
+
+      context "+ list" do
+        it "should have list as type" do
+          OrgMob::Parser.parse("+ List").first[:type].should eq("list")
+        end
+      end
+
+      context "1) list" do
+        it "should have list as type" do
+          OrgMob::Parser.parse("1) List").first[:type].should eq("list")
+        end
+      end
+
+      context "1. list" do
+        it "should have list as type" do
+          OrgMob::Parser.parse("1. List").first[:type].should eq("list")
+        end
+      end
+    end
   end
 end
