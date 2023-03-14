737e1492109ec9f0f8100641c5d38040725ea57c
roy-corentin <corentin.roy@epitech.eu>
2023-03-14 22:21:14 +0100

    [UP] simplify hash and lexer method

--- src/lexer.cr
+++ src/lexer.cr
@@ -5,20 +5,10 @@ module OrgMob
   alias LexerFunctionType = Proc(String, NamedTuple("type": Symbol, "content": String))
 
   class Lexer
-    @@lexers : Hash(Regex, LexerFunctionType) = {TITLE_REGEX => ->OrgMob::Lexer.title(String), LIST_REGEX => ->OrgMob::Lexer.list(String)}
+    @@lexers : Hash(Regex, LexerFunctionType) = {TITLE_REGEX => ->title(String), LIST_REGEX => ->list(String)}
 
     def self.call(data : Array(String)) : Array(Lexed)
-      output = [] of Lexed
-      data.each_with_object(output, &self.call_appropriate_lexer)
-      return output
-    end
-
-    private def self.call_appropriate_lexer
-      ->(line : String, array : Array(Lexed)) { REGEXS.each { |regex| array << @@lexers[regex].call(line) if line.match(regex) } }
-    end
-
-    private def self.parse_todos(data : Array(String), todo_keywords : Array(String))
-      regex = /^\*+\s(#{todo_keywords.join("|")})\s.*/
+      return data.each_with_object([] of Lexed, &self.call_appropriate_lexer)
     end
 
     def self.title(title_content : String) : Lexed
@@ -31,6 +21,10 @@ module OrgMob
 
     def self.property(property_content : String) : Lexed
       {type: :property, content: property_content}
+    end
+
+    private def self.call_appropriate_lexer
+      ->(line : String, array : Array(Lexed)) { REGEXS.each { |regex| array << @@lexers[regex].call(line) if line.match(regex) } }
     end
   end
 end
