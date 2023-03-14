fe0784a6e09201cfdb63c81f6205eba129e75ae5
roy-corentin <corentin.roy@epitech.eu>
2023-03-14 16:57:19 +0100

    [UP] Transform missing format to lexer

--- src/lexer.cr
+++ src/lexer.cr
@@ -3,15 +3,15 @@ require "./regex"
 module OrgMob
   alias Lexed = NamedTuple("type": Symbol, "content": String)
 
-  LEXERS = {TITLE_REGEX => ->OrgMob::Format.format_title(String), LIST_REGEX => ->OrgMob::Format.format_list(String)}
+  LEXERS = {TITLE_REGEX => ->OrgMob::Lexer.title(String), LIST_REGEX => ->OrgMob::Lexer.list(String)}
 
-  class Format
-    def self.format_title(title_content : String) : Lexed
+  class Lexer
+    def self.title(title_content : String) : Lexed
       level = title_content.count('*')
       {type: :title, content: title_content}
     end
 
-    def self.format_list(list_content : String) : Lexed
+    def self.list(list_content : String) : Lexed
       {type: :list, content: list_content}
     end
   end
