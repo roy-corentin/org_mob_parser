cab30f7409ab833719110b300848c9c918986141
roy-corentin <corentin.roy@epitech.eu>
2023-03-14 10:10:16 +0100

    [ADD] List regex

--- src/org_mob_regex.cr
+++ src/org_mob_regex.cr
@@ -1,6 +1,6 @@
 module OrgMob
   TITLE_REGEX = /^\*+\s(.*)/im
-  LIST_REGEX  = /^(-|\+|[0-9]\.|[0-9]\))\s(.*)/im
+  LIST_REGEX  = /^(-|\+|[0-9](\.|\)))\s(.*)/im
 
-  REGEXS = [TITLE_REGEX]
+  REGEXS = [TITLE_REGEX, LIST_REGEX]
 end
