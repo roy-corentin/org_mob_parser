1dd6d2e02b639c81296067a52f1450611532909a
roy-corentin <corentin.roy@epitech.eu>
2023-03-19 22:26:24 +0100

    [UP] rename function

--- src/lexer.cr
+++ src/lexer.cr
@@ -7,14 +7,14 @@ module OrgMob
     alias LexerFunctionType = Proc(String, NamedTuple("type": Symbol, "content": String))
 
     def self.call(data : Array(String)) : Array(Lexed)
-      return data.each_with_object([] of Lexed, &self.call_appropriate_lexer)
+      return data.each_with_object([] of Lexed, &self.call_lexer_with_appropriate_parameters)
     end
 
     def self.format(type : Symbol, content : String, match : Regex::MatchData) : Lexed
       {type: type, content: content, match: match}
     end
 
-    private def self.call_appropriate_lexer
+    private def self.call_lexer_with_appropriate_parameters
       ->(line : String, array : Array(Lexed)) do
         REGEXS.each do |regex|
           if match_data = line.match(regex[:regex])
