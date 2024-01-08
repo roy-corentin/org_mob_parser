c02c60b2c9b0880b1e8c9c87629cc1274b7e45a2
roy-corentin <corentin.roy@epitech.eu>
2024-01-08 10:54:27 +0100

    [UP] lexer do not use map and no lambda anymore

--- src/lexer.cr
+++ src/lexer.cr
@@ -7,19 +7,14 @@ module OrgMob
     alias LexerFunctionType = Proc(String, NamedTuple("type": Symbol, "content": String))
 
     def call(data : Array(String)) : Array(Lexed)
-      result = [] of Lexed
-      data.each_with_object(result, &tokenize_string)
+      data.map { |line| tokenize_string(line) }.compact
     end
 
-    private def tokenize_string
-      ->(line : String, result : Array(Lexed)) do
-        match_data = nil
-        regex = TOKENS.find { |token| (match_data = line.match(token[:regex])) }
+    private def tokenize_string(line : String) : Lexed?
+      match_data = nil
+      regex = TOKENS.find { |token| (match_data = line.match(token[:regex])) }
 
-        return if regex.nil? || match_data.nil?
-
-        result << {type: regex[:type], content: line, match: match_data}
-      end
+      (regex && match_data) ? {type: regex[:type], content: line, match: match_data} : nil
     end
   end
 end
